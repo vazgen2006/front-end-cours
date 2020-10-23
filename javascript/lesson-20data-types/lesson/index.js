f215abf8968dc3a47617c8b31d4d550bbb4ce86d
vazgen2006 <sargsyanvazgen2006@gmail.com>
2020-10-23 20:54:02 +0400

    javascript

---
+++ javascript/lesson-20data-types/lesson/index.js
@@ -0,0 +1,39 @@
+    /** 
+     * JS data types
+     * (tvyalneri tiper)
+    */
+    
+    //Dinamic(tuyl) & Static(hastatun)
+
+    // 1 Primitive types
+    // 2 nunber (..., NaN, Infinity, -Infinity)
+    // 3 string
+    // 4 boolean(true, false)
+    // 5 undefined
+    //  null
+    // +-
+
+    "use strict"
+
+    /**
+     * Operators
+     */
+
+     // 1. unar
+     // 2. binar (+, -, *, /, %) (= +=, -=, *=, /=, %=,), (>, <, >=, <=, ==, !=, ===, =!), (||, &&), (|, &)
+     // 3. ternar
+    
+    var d = 10
+
+     d = d + 5
+
+    // && (true && true) = true
+    // && (true && false) = false
+    // && (false && true) = false
+    // && (false && false) = false
+
+
+    // || (true || true) = true
+    // || (true || false) = true
+    // || (false || true) = true
+    // || (false || false) = false
